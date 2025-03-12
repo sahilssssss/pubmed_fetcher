798e63924228c804566b667618d3b0b47e032c12
Sahil Dey <139077358+sahilssssss@users.noreply.github.com>
2025-03-12 12:41:31 +0530

    final commit constants.py

--- src/pubmed_paper_fetcher/constants.py
+++ src/pubmed_paper_fetcher/constants.py
@@ -1,3 +1,15 @@
+import os 
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+API_KEY = os.getenv("PUBMED_API_KEY")
+
+# Handle missing API key case
+if not API_KEY:
+    raise ValueError("Missing PUBMED_API_KEY! Set it in your .env file.")
+
+
 # PubMed API Endpoints
 BASE_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
 SEARCH_URL = f"{BASE_URL}/esearch.fcgi"  
@@ -5,15 +17,20 @@ FETCH_URL = f"{BASE_URL}/efetch.fcgi"
 
 # Keywords to Identify Non-Academic Companies
 COMPANY_KEYWORDS = [
-    "Pharma", "Biotech", "Inc.", "Ltd", "Corporation", "Laboratories", "Synapse",
-    "Genomics", "Therapeutics", "Biosciences", "Pathology","Diagnostics", "Life Sciences",
-    "Biomedical", "Biopharma", "Research Institute", "Technologies","Laboratory",
+    "Pharma", "Biotech", "Inc.", "Ltd", "Corporation", "Laboratories",
+    "Genomics", "Therapeutics", "Biosciences", "Diagnostics", "Life Sciences",
+    "Biomedical", "Biopharma", "Research Institute", "Technologies",
     "Neurosciences", "Health Solutions", "GmbH", "LLC", "S.A.", "Pvt Ltd"
 ]
 
 # API Configuration
 DEFAULT_MAX_RESULTS = 50  
-REQUEST_TIMEOUT = 10       
+REQUEST_TIMEOUT = 10   
+
+
+# API Rate Limit Handling
+MAX_RETRIES = 5  # Maximum retries when hitting rate limits
+RETRY_BACKOFF = 2  # Exponential backoff multiplier (in seconds)
 
 # CSV File Configurations
 DEFAULT_CSV_FILENAME = "pubmed_papers.csv"
